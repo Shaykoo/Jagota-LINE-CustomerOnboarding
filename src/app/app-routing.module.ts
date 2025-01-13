5797805c8b823d3f44f69e7efd927065fe219c5d
Abhishek Sharma <shaykoo1993@gmail.com>
2025-01-13 17:36:03 +0800

    added OTP module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: '/registration', pathMatch: 'full' },
   { path: 'registration', loadChildren: () => import('./features/registration/registration.module').then(m => m.RegistrationModule) },
-  { path: 'otp', loadChildren: () => import('./features/otp/otp.module').then(m => m.OtpModule) }
+  { path: 'otp', loadChildren: () => import('./features/otp/otp.module').then(m => m.OtpModule) },
+  { path: 'complete', loadChildren: () => import('./features/complete/complete.module').then(m => m.CompleteModule) }
 ];
 
 @NgModule({
